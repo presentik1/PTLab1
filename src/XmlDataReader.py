382e8e5e3e4740fb30b9f624b4750aca268bc27f
Astafev Artem Viktorovich <presentik11@gmail.com>
2024-11-22 00:53:37 +0300

    fixed tests

--- src/XmlDataReader.py
+++ src/XmlDataReader.py
@@ -1,45 +1,42 @@
 from src.DataReader import DataReader
 from src.Types import DataType
 from lxml import etree as ET
+from charset_normalizer import from_path  # Для автоматического определения кодировки
 
 
 class XmlDataReader(DataReader):
     def read(self, path: str) -> DataType:
         students = {}
         try:
-            with open(path, 'r', encoding='windows-1251') as f:  # Используйте
-                tree = ET.parse(f)  # Открытие файла
+            # Определение кодировки файла
+            detected_encoding = from_path(path).best().encoding
+
+            with open(path, 'r', encoding=detected_encoding) as f:
+                tree = ET.parse(f)  # Парсинг XML файла
                 root = tree.getroot()
         except ET.XMLSyntaxError as e:
-            raise ValueError(
-                f"Ошибка при парсинге XML: {e}"
-            )
+            raise ValueError(f"Ошибка при парсинге XML: {e}")
         except UnicodeDecodeError as e:
-            raise ValueError(
-                f"Ошибка кодировки: {e}"
-            )
+            raise ValueError(f"Ошибка кодировки: {e}")
+        except Exception as e:
+            raise ValueError(f"Произошла непредвиденная ошибка: {e}")
 
         for student in root.findall('student'):
             student_name = student.get('name')
             if not student_name:
-                raise ValueError(
-                    "Не найдено имя студента в атрибуте 'name'."
-                )
+                raise ValueError("Не найдено имя студента в атрибуте 'name'.")
 
             subjects = []
             for subject in student.findall('subject'):
                 subject_name = subject.get('name')
                 if not subject_name:
-                    raise ValueError(
-                        "Не найдено название предмета в атрибуте 'name'."
-                    )
+                    raise ValueError("Не найдено название предмета в атрибуте 'name'.")
 
                 try:
                     score = int(subject.text)
-                except ValueError:
+                except (ValueError, TypeError):
                     raise ValueError(
-                        f"Ошибка при изменение баллов в число для предмета: "
-                        f"{subject_name}"
+                        f"Ошибка преобразования баллов в число для предмета: {subject_name}"
                     )
 
                 subjects.append((subject_name, score))
