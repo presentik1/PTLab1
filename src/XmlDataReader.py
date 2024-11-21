9caf3d5ac199cd7a3a861c6622db687e6b2ea5da
Astafev Artem Viktorovich <presentik11@gmail.com>
2024-11-22 00:56:55 +0300

    fixed tests

--- src/XmlDataReader.py
+++ src/XmlDataReader.py
@@ -1,7 +1,7 @@
 from src.DataReader import DataReader
 from src.Types import DataType
 from lxml import etree as ET
-from charset_normalizer import from_path  # Для автоматического определения кодировки
+from charset_normalizer import from_path
 
 
 class XmlDataReader(DataReader):
@@ -15,28 +15,39 @@ class XmlDataReader(DataReader):
                 tree = ET.parse(f)  # Парсинг XML файла
                 root = tree.getroot()
         except ET.XMLSyntaxError as e:
-            raise ValueError(f"Ошибка при парсинге XML: {e}")
+            raise ValueError(
+                f"Ошибка при парсинге XML: {e}"
+            )
         except UnicodeDecodeError as e:
-            raise ValueError(f"Ошибка кодировки: {e}")
+            raise ValueError(
+                f"Ошибка кодировки: {e}"
+            )
         except Exception as e:
-            raise ValueError(f"Произошла непредвиденная ошибка: {e}")
+            raise ValueError(
+                f"Произошла непредвиденная ошибка: {e}"
+            )
 
         for student in root.findall('student'):
             student_name = student.get('name')
             if not student_name:
-                raise ValueError("Не найдено имя студента в атрибуте 'name'.")
+                raise ValueError(
+                    "Не найдено имя студента в атрибуте 'name'."
+                )
 
             subjects = []
             for subject in student.findall('subject'):
                 subject_name = subject.get('name')
                 if not subject_name:
-                    raise ValueError("Не найдено название предмета в атрибуте 'name'.")
+                    raise ValueError(
+                        "Не найдено название предмета в атрибуте 'name'."
+                    )
 
                 try:
                     score = int(subject.text)
                 except (ValueError, TypeError):
                     raise ValueError(
-                        f"Ошибка преобразования баллов в число для предмета: {subject_name}"
+                        "Ошибка преобразования баллов в число "
+                        f"для предмета: {subject_name}"
                     )
 
                 subjects.append((subject_name, score))
