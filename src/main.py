30f099f72a7fd2ebe18d6e8ae5dc22bb3baaf593
Astafev Artem Viktorovich <presentik11@gmail.com>
2024-11-22 23:23:50 +0300

    fixed main

--- src/main.py
+++ src/main.py
@@ -4,8 +4,9 @@ import sys
 
 from CalcRating import CalcRating
 from TextDataReader import TextDataReader
+from PerfectScorer import PerfectScorer  # Импортируем PerfectScorer
 
-
+# Функция для получения пути из аргументов командной строки
 def get_path_from_arguments(args) -> str:
     parser = argparse.ArgumentParser(description="Path to datafile")
     parser.add_argument(
@@ -15,17 +16,25 @@ def get_path_from_arguments(args) -> str:
     args = parser.parse_args(args)
     return args.path
 
-
 def main():
+    # Получаем путь к файлу данных
     path = get_path_from_arguments(sys.argv[1:])
 
+    # Чтение данных
     reader = TextDataReader()
     students = reader.read(path)
     print("Students: ", students)
 
+    # Вычисляем рейтинг студентов
     rating = CalcRating(students).calc()
     print("Rating: ", rating)
 
+    # Ищем студентов с 100 баллами по всем дисциплинам
+    scorer = PerfectScorer(students)  # Создаем объект PerfectScorer
+    perfect_student = scorer.find_perfect_student()  # Находим такого студента
+
+    # Выводим результат
+    print(perfect_student)
 
 if __name__ == "__main__":
     main()
